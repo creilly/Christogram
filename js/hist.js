c0329efac94be90ccca4f4bb65393e3119f3fad5
Christopher Reilly <reilly.christopher@gmail.com>
2012-10-15 13:42:46 -0500

    layout/controls finished for now, started plotting

--- js/hist.js
+++ js/hist.js
@@ -1,4 +1,4 @@
-var height = 400, width = 800;
+var height = 500, width = 700;
 var pad = .2;
 var barPad = .2;
 var tickPad = .05;
@@ -16,400 +16,475 @@ var plots = null;
 //called when creating new plot
 var plotCounter = 1;
 
-function createPlot(data){
+function createPlot(data) {
 
-    var plotName = 'plot' + plotCounter.toString();
-    
-    var plot = $('<div></div>')
-	.addClass('accordion-group')
-	.addClass('alert')
-	.addClass('fade')
-	.addClass('in')
-	.prop('id',plotCounter)
-	.data('data',data);
+	//@off
 
-    var close = $('<a><i class="icon-remove" /></a>')
-	.addClass('close')
-	.attr('data-dismiss','alert');
+	var plotName = 'plot' + plotCounter.toString();
 
-    plot.append(close);
+	var plot = $('<div>')
+		.addClass('accordion-group')
+		.addClass('alert')
+		.addClass('fade')
+		.addClass('in')
+		.addClass('plot')
+		.data('data', data.sort(function(a,b){return a - b}));
 
-    var heading = $('<div></div>')
-	.addClass('accordion-heading')
-	.addClass('container-fluid');
+	var close = $('<a><i class="icon-remove" /></a>')
+		.addClass('close')
+		.attr('data-dismiss', 'alert');
 
-    var headingrow = $('<div></div>')
-	.addClass('row-fluid');
-    
-    var active = $('<input />')
-	.addClass('span3')
-	.prop('type','checkbox')
-	.prop('checked','true')
+	plot.append(close);
 
-    headingrow.append(active);
-    
-    var toggle = $('<a></a>')
-	.addClass('accordion-toggle')
-	.addClass('span9')
-	.attr('href','#' + plotName)
-	.attr('data-parent','#' + plots.attr('id'))
-	.attr('data-toggle','collapse')			
-	.html(plotName);
+	var heading = $('<div>')
+		.addClass('accordion-heading')
+		.addClass('container-fluid');
 
-    headingrow.append( toggle );
+	var headingrow = $('<div></div>')
+		.addClass('row-fluid');
 
-    heading.append(headingrow);
+	var active = $('<input />')
+		.addClass('span3')
+		.addClass('active')
+		.prop('type', 'checkbox')
+		.prop('checked', 'true');
 
-    plot.append(heading);
+	headingrow.append(active);
 
-    var body = $('<div></div>')
-	.addClass('accordion-body')
-	.addClass('collapse')
-	.attr('id',plotName);
+	var toggle = $('<a>')
+		.addClass('accordion-toggle')
+		.addClass('span9')
+		.addClass('plot-title')
+		.attr('href', '#' + plotName)
+		.attr('data-parent', '#' + plots.attr('id'))
+		.attr('data-toggle', 'collapse')
+		.html(plotName);
 
-    var properties = $('<form></form>')
-	.addClass('accordion-inner');
+	headingrow.append(toggle);
 
-    properties.append( $('<label>' + 'Plot Name' + '</label>') );
-			           
-    var title = $('<input />')
-	.prop('type','text')
-	.prop('id','title')
-	.prop('value',plotName);
-    
-    properties.append(title);
+	heading.append(headingrow);
 
-    properties.append( $('<label>' + 'bin number' + '</label>' ) );
+	plot.append(heading);
 
-    var binnumber = $('<input />')
-	.prop('type','number')
-	.prop('step',1)
-	.prop('value',50);
-    
-    properties.append(binnumber);
+	var body = $('<div>')
+		.addClass('accordion-body')
+		.addClass('collapse')
+		.attr('id', plotName);
 
-    properties.append( $('<label>' + 'color' + '</label>') );
+	var properties = $('<form>')
+		.addClass('accordion-inner');
 
-    var color = $('<input />')
-	.prop('id','color-edit')
-	.prop('type','color');
+	properties.append($('<label>' + 'Plot Name' + '</label>'));
 
-    properties.append( color );
+	var title = $('<input />')
+		.addClass('title')
+		.prop('type', 'text')
+		.prop('value', plotName);
 
-    body.append( properties );
-    
-    plot.append( body );
+	properties.append(title);
 
-    //handle logic
+	properties.append($('<label>' + 'bin number' + '</label>'));
 
-    plot.bind('close', function () { console.log( plotName + ' was closed' ) } );
+	var binnumber = $('<input />')
+		.addClass('binnumber')
+		.prop('type', 'number')
+		.prop('step', 1)
+		.prop('value', 50);
 
-    active.change( function () {console.log(plotName + (this.checked ? ' is active' : ' is inactive' ))} );
+	properties.append(binnumber);
 
-    active.trigger('change');
+	properties.append($('<label>' + 'color' + '</label>'));
 
-    title.keyup( function (e) {
-	    if (e.keyCode == 13) {
-		$('.accordion-toggle',plot).html(this.value);
-	    }
+	var color = $('<input />')
+		.addClass('color')
+		.prop('type', 'color');
+
+	properties.append(color);
+
+	body.append(properties);
+
+	plot.append(body);
+	
+	//@on
+
+	//handle logic
+
+	plot.bind('close', plotRemoved);
+
+	// start shown
+	active.trigger('change');
+	// hide/show
+	active.change(activeChanged);
+
+	// change title on enter pressed
+	title.keyup(function(e) {
+		if (e.keyCode == 13) {
+			titleChanged(this);
+		}
+	});
+
+	// binnumber
+	binnumber.prop('value',50)
+	binnumber.change(binnumberChanged)
+
+	//color
+	color.change(colorChanged);
+
+	var initColor = '#';
+
+	var rgb = hslToRgb(((23 + 3 * plotCounter ) / 13 ) % 1, .5, .5);
+
+	for (var i in rgb) {
+		initColor += Math.round(rgb[i]).toString(16);
 	}
-	); 
 
-    color.change( function () {
-	    $('.accordion-toggle',plot).css('color',this.value);
-	}
-	);
+	color.prop('value', initColor);
 
-    var initColor = '#';
+	color.trigger('change');
 
-    var rgb = hslToRgb( ( ( 23 + 3 * plotCounter ) / 13 ) % 1 , .5, .5 );
+	plot.addClass('initialized');
 
-    for (var i in rgb) {
-	initColor += Math.round(rgb[i]).toString(16);
-    }
+	plots.append(plot);
 
-    color.prop('value',initColor);
+	toggle.trigger('click.collapse.data-api');
+	
+	updateCanvas();
 
-    color.trigger('change');
-
-    plots.append(plot);
-
-    toggle.trigger('click.collapse.data-api');
-
-    plotCounter++;
+	plotCounter++;
 };
 
-function hslToRgb(h, s, l){
-    var r, g, b;
+function activeChanged() {
+	updateCanvas();
+}
 
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
+function binnumberChanged() {
+	var plot = $(this).parents('.plot');
+	if (boolUpdate(plot)) {
+		updateCanvas();
+	}
+}
 
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
+function colorChanged() {
+	var plot = $(this).parents('.plot');
+	$('.plot-title', plot).css('color', this.value);
+	if (boolUpdate(plot)) {
+		updateCanvas();
+	}
+		
+}
+
+function titleChanged(el) {
+	var plot = $(el).parents('.plot');
+	$('.plot-title', plot).text(el.value);
+}
+
+function plotRemoved() {
+	updateCanvas();
+}
+
+function boolUpdate(plot) {
+	var result = plot.hasClass('initialized');
+	result = result && $('.active', plot).is(':checked');
+	return result
+}
+
+function updateCanvas() {
+	var plots = $('.plot').has('.active:checked')
+	var min = 'null';
+	var max = 'null'; 
+	plots.each(function () {
+		plotData = $(this).data('data');
+		var plotMin = plotData[0];
+		var plotMax = plotData[plotData.length - 1]
+		if (min == 'null') {
+			min = plotMin;
+			max = plotMax;
+			return
+		}
+		min = (plotMin - min > 0) ? min : plotMin
+		max = (plotMax - max > 0) ? plotMax : max
+	});
+
+	var min_raw = parseInt($('#x-min').prop('value')); 
+	var max_raw = parseInt($('#x-max').prop('value'));
+	var min = min + min_raw / 100 * max_raw / 100 * (max-min);
+	var max = min + max_raw / 100 * (max - min);
+	
+	canvas.width = canvas.width;
+	
+	plots.each(function () {
+		var plot = $(this);
+		var hist = binData(plot.data('data'),parseInt(plot.find('.binnumber').prop('value')),min,max);
+		var histMax = Math.max.apply(null,hist);
+		var color = hexToRgb(plot.find('.color').prop('value'));
+		
+		c.fillStyle = 'rgba(' + color + ',.7)'
+		c.beginPath();
+		for (var iii in hist) {
+			c.rect(1 + iii/hist.length * canvas.width, ( 1 - hist[iii] / histMax ) * canvas.height, canvas.width / parseInt(plot.find('.binnumber').prop('value')) - 2,  hist[iii] / histMax * canvas.height);
+		}
+		c.closePath();
+		c.fill();
+	});
+}
+
+function hexToRgb(hex) {
+	rgb = [];
+	var iii = 0;
+    while (iii < 3) {
+    	rgb.push(parseInt(hex.substring(2 * iii + 1, 2 * iii + 3),16).toString());
+    	iii++;
     }
-
-    return [r * 255, g * 255, b * 255];
+    return rgb.join(',');
 }
 
 // scaling functions.  input ranges from 0 to 1.  origin is bottom left.
-function x ( d, p ) {
-    if (p) {
-	return width * ( ( 1 - pad ) * d + pad / 2 );
-    }
-    else {
-	return width * ( 1 - pad ) * d;
-    }
+function x(d, p) {
+	if (p) {
+		return width * ((1 - pad ) * d + pad / 2 );
+	} else {
+		return width * (1 - pad ) * d;
+	}
 };
 
-function y ( d, p ) {
-    if (p) {
-	return height * ( ( 1 - pad / 2 ) - ( 1 - pad ) * d );
-    }
-    else {
-	return ( 1 - pad ) * height * d;
-    }
+function y(d, p) {
+	if (p) {
+		return height * ((1 - pad / 2 ) - (1 - pad ) * d );
+	} else {
+		return (1 - pad ) * height * d;
+	}
 };
 
 function drawBBox() {
-    var thickness = 5;
-    c.lineWidth = 2 * thickness;
-    c.rect(thickness,thickness, width - 2 * thickness, height - 2 * thickness);
-    c.stroke();
-};
-
-
-//produce tick mark locations from endpoints and desired density
-function getTickCoords(min, max) {
-    var range = max - min;
-    var tickWidth = Math.pow(10, Math.ceil( Math.log(range) / Math.log(10) ) - 1);
-    
-    var tick = Math.ceil(min/tickWidth) * tickWidth;
-    var ticks = [];
-    while (tick < max){
-	ticks.push(tick);
-	tick += tickWidth;
-    }
-    return ticks;
+	var thickness = 5;
+	c.lineWidth = 2 * thickness;
+	c.rect(thickness, thickness, canvas.width - 2 * thickness, canvas.height - 2 * thickness);
+	c.stroke();
 };
 
 function activePlot() {
-    return plots[plotSelect.selectedIndex];
+	return plots[plotSelect.selectedIndex];
 };
 
 // get an array of n gaussian distributed (approx) random numbers from 0 to 1
-function gaussian (n, mean, spread) {
-    
-    var ran = Math.random;
+function gaussian(n, mean, spread) {
 
-    var data = [];
+	var ran = Math.random;
 
-    for (var i = 0; i < n; i++){
-	data[i] = ( (ran() + ran() + ran()) / 3 - .5 ) * spread + mean;
-    }
-    
-    return data;
-};
+	var data = [];
 
-function drawAxes () {
-    r.path ( 'M' + x(0,true).toString() + ',' + y(1,true).toString() + 
-	     'L' + x(0,true).toString() + ',' + y(0,true).toString() + 
-	     'L' + x(1,true).toString() + ',' + y(0,true).toString() );
+	for (var i = 0; i < n; i++) {
+		data[i] = ((ran() + ran() + ran()) / 3 - .5 ) * spread + mean;
+	}
+
+	return data;
 };
 
 // returns a function that will find the bin number for you
-function binner ( bins, start, end ) {
-    return function ( x ) {
-	return Math.floor( ( x - start ) * bins / ( end - start ) )
-    };
+function binner(bins, start, end) {
+	return function(x) {
+		return Math.floor((x - start ) * bins / (end - start ))
+	};
 };
 
 // creates a binned array from data and number of bins
-function binData( data, bins ) {
-    
-    var start = data[0];
-    var end = data[data.length -1];
+function binData(data, bins, min, max) {
 
-    var bin = binner( bins, start, end );
-    
-    var hist = [];
-    
-    for (var i = 0; i <= bins; i++){
-	hist[i] = 0;
-    }
+	var bin = binner(bins, min, max);
 
-    for (var i = 0; i < data.length; i++){
-	hist[bin(data[i])]++;
-    }
+	var hist = [];
 
-    return hist;
+	for (var i = 0; i <= bins; i++) {
+		hist[i] = 0;
+	}
 
-};
+	for (var i = 0; i < data.length; i++) {
+		if ( ( data[i] >= min ) && ( data[i] <= max ) ) {
+			hist[bin(data[i])]++;
+			}
+	}
 
-//add select option, returns option's index
-function newOption (name) {
-    var option = document.createElement('option');
-    option.text = name;
-    plotSelect.add(option, null);
-    return plotSelect.options.length - 1;
-}
+	return hist;
 
-// queries the slider and draws a histogram
-function binsChanged () {
-
-    var plot = activePlot();
-
-    var bins = binSlider.valueAsNumber;
-
-    var hist = binData( plot.data, bins );
-
-    plot.rawHist = hist;
-
-    console.log('bins changed: ' + bins.toString() );
 };
 
 function changeHistogramValues(plot, hist) {
-    var bins = hist.length;
 
-    clearSet(plot.bars);
-
-    plot.bins = bins;
-
-    plot.hist = hist;
-
-    var max = Math.max.apply(null, hist);
-
-    var binWidth = x( 1 / bins, false );
-    
-    var barWidth = binWidth * ( 1 - barPad );
-    var barOffset = (binWidth - barWidth) / 2;
-    
-    for (var i = 0; i < bins; i++) {
-    
-    var height = hist[i] / max;
-    var yLoc = y((plotSelect.selectedIndex) ? height : 1,true);
-    var barHeight = y(height, false);
-    
-    var xLoc = x( i / bins, true ) + barOffset;
-    
-    var rect = r.rect( xLoc, yLoc, barWidth, barHeight )    
-    plot.bars.push(rect);
-    }
-    
-    plot.bars.attr('fill',colorWheel.value);
-    plot.bars.attr('fill-opacity', .5);
-    plot.bars.attr('stroke-width',0);
-    
-    if ( plot.axes[1] != null ){
-    clearSet(plot.axes[1].ticks);
-    plot.axes[1] = null;
-    }
-    drawTicks(horizontal = false);
+	
 }
 
-//queries color slider and sets outline color of bars
-function colorChanged () {
-    var plot = activePlot();
-    var color = colorWheel.value;
-    plot.color = colorWheel.value;
-    plot.bars.attr('fill',color);
-    plot.axes[0].ticks.attr('fill',color);
-    plot.axes[1].ticks.attr('fill',color);
-};
-
 function parseString(s) {
-    if ( s.search(/^[\d.]+|[\s,]+$/) == 0 ) {
-	var data = s.match(/[\d.]+/g);
-	for (var i = 0; i < data.length; i++) {
-	    data[i] = parseFloat(data[i]);
+	if (s.search(/^[\d.]+|[\s,]+$/) == 0) {
+		var data = s.match(/[\d.]+/g);
+		for (var i = 0; i < data.length; i++) {
+			data[i] = parseFloat(data[i]);
+		}
+		return data;
+	} else {
+		return -1;
 	}
-	return data;
-    }
-    else {
-	return -1;
-    }
 };
 
 function handleFileSelect(event) {
-    
-    console.log('drop detected');
 
-    event.stopPropagation();
-    event.preventDefault();
-    
-    //just get first file
-    var file = event.dataTransfer.files[0]; // FileList object
+	console.log('drop detected');
 
-    var fileName = file.name;
+	event.stopPropagation();
+	event.preventDefault();
 
-    var reader = new FileReader();
+	//just get first file
+	var file = event.dataTransfer.files[0];
+	// FileList object
 
-    function onRead (event) {
-	
-	var dataString = event.target.result;
+	var fileName = file.name;
 
-	var data = parseString(dataString);
+	var reader = new FileReader();
 
-	console.log(data);
+	function onRead(event) {
 
-	if (data == -1) {
-	    //improper data format
-	    console.log('improper data format');
-	    return; 
-	}
+		var dataString = event.target.result;
 
-	createPlot(newPlot(data, fileName));
-    };
+		var data = parseString(dataString);
 
-    // Closure to capture the file information.
-    reader.onload = onRead;
+		console.log(data);
 
-    // Read in the image file as a data URL.
-    reader.readAsText(file);
+		if (data == -1) {
+			//improper data format
+			console.log('improper data format');
+			return;
+		}
 
-    return false;
+		createPlot(newPlot(data, fileName));
+	};
+
+	// Closure to capture the file information.
+	reader.onload = onRead;
+
+	// Read in the image file as a data URL.
+	reader.readAsText(file);
+
+	return false;
 };
 
 function handleDragOver(event) {
-    console.log('drag detected');
-    event.stopPropagation();
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-    return false;
+	console.log('drag detected');
+	event.stopPropagation();
+	event.preventDefault();
+	event.dataTransfer.dropEffect = 'copy';
+	// Explicitly show this is a copy.
+	return false;
 };
 
-$( 
-  function () {
-      
-      canvas = document.getElementById('hist');
+function initializeControls() {
 
-      canvas.width = width;
+	//set slider ranges
+	$('input[type=range]').prop('min', 0).prop('max', 100);
+	$('#x-min').prop('value', 0);
+	$('#x-max').prop('value', 100);
+	$('#size, #aspect').prop('value', 50);
 
-      canvas.height = height;
+	//size or aspect routes to same function
+	$('#size, #aspect').change(dimsChanged);
 
-      c = canvas.getContext('2d');
-      
-      drawBBox();
+	//change x-range
+	$('#x-min, #x-max').change(domainChanged)
 
-      plots = $('#plots').first();
+	//add plot button
+	$('#add-plot').click(function() {
+		if (!plots.hasClass('in')) {
+			console.log('here');
+			plots.prev().children().click();
+		}
+		createPlot(gaussian(10000, 1, 100));
+	});
 
-      $('input[type=range]').prop('min', 20);
-      $('input[type=range]').prop('max', 40);
-      
+};
 
-      $('#add-plot').click( function () {createPlot(gaussian(100,1,100))} );
-  }
-   );
+//change x-range
+function domainChanged() {
+	updateCanvas();
+}
+
+//change canvas dimensions
+function dimsChanged() {
+	var aspect = $('#aspect').prop('value');
+	var size = $('#size').prop('value');
+	canvas.width = width * size / 50 * (.3 + aspect / 100 * .4 ) * 2;
+	canvas.height = height * size / 50 * (.3 + (100 - aspect ) / 100 * .4 ) * 2;
+	updateCanvas();
+}
+
+//utility for generating formatted controls
+function controlGroup(name, type) {
+	//@off
+	return $('<div>')
+		.addClass('control-group')
+		.append(
+			$('<label>')
+			.addClass('control-label')
+			.prop('for', name)
+			.html(name))
+			.append(
+				$('<div>')
+				.addClass('controls')
+				.append(
+					$('<input />')
+					.prop('id', name)
+					.prop('type', type)
+				)
+			);
+	//@on
+}
+
+$(function() {
+
+	canvas = document.getElementById('hist');
+
+	canvas.width = width;
+
+	canvas.height = height;
+
+	c = canvas.getContext('2d');
+
+	plots = $('#plots');
+
+	dimsChanged();
+
+	initializeControls();
+	
+	createPlot(gaussian(100000,30,4.0));
+	
+	createPlot(gaussian(100000,31,5.0));
+});
+
+function hslToRgb(h, s, l) {
+	var r, g, b;
+
+	if (s == 0) {
+		r = g = b = l;
+		// achromatic
+	} else {
+		function hue2rgb(p, q, t) {
+			if (t < 0)
+				t += 1;
+			if (t > 1)
+				t -= 1;
+			if (t < 1 / 6)
+				return p + (q - p) * 6 * t;
+			if (t < 1 / 2)
+				return q;
+			if (t < 2 / 3)
+				return p + (q - p) * (2 / 3 - t) * 6;
+			return p;
+		}
+
+		var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		var p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1 / 3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1 / 3);
+	}
+
+	return [r * 255, g * 255, b * 255];
+}
 
